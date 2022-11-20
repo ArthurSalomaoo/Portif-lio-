b7a3c418b1c9fe2207b5ed95e687c2aafa5a7c7b
Arthur Salomao <artursalomao004@gmail.com>
2022-11-20 00:19:21 -0300

    alguns ajustes no css, e javascripit 95% terminado

--- Countdown/script.js
+++ Countdown/script.js
@@ -1,1 +1,33 @@
-"use strict"
+//"use strict"
+
+let dd = 8
+let hh = 12
+let mm = 44
+let ss = 28
+let mile = 1000
+
+function contagem() {
+    setInterval(() => {    ss--
+    
+        if(ss == 0){
+            ss = 60
+            mm--
+        }
+        if(mm == 0){
+            mm = 60
+            hh--
+        }
+        if(hh == 0){
+            hh = 24
+            if(dd > 0){
+                dd--
+            }
+        }
+        
+        
+        
+        let formato = (dd < 10 ? '0' + dd : dd) + ':' + (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss )    
+        document.getElementById('tempo').innerHTML = formato
+     }, mile)
+}
+
