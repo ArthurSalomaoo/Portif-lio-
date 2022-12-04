d48b4f4dd1844a8553aae1883aaff4e36712d686
Arthur Salomao <artursalomao004@gmail.com>
2022-12-04 20:04:05 -0300

    parte da api 90% terminada

--- Rocketflix/main.js
+++ Rocketflix/main.js
@@ -9,13 +9,17 @@ function getFilme(){
     let id = Math.floor(Math.random() * 1000)
 
     const URL = `https://api.themoviedb.org/3/movie/${id}?api_key=52049f468d48721a1489c682170d7877`;
-    const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+    const IMG_URL = 'https://image.tmdb.org/t/p/w500';
     const language = 'language=pt-BR';
 
     axios.get(URL)
     .then(data => {
         console.log(data.data)
         document.getElementById('filme').style = 'display: block'
+        document.getElementById('fotoFilme').setAttribute('src', IMG_URL + data.data.poster_path)
+        document.getElementById('tituloFilme').innerHTML = data.data.original_title
+        document.getElementById('sinopse').innerHTML = data.data.overview
+
         
     })
     .catch(error => {
