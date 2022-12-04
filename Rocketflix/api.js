a14149e69cd876db61cb43ba548ffa86554ee7fd
Arthur Salomao <artursalomao004@gmail.com>
2022-12-04 19:39:40 -0300

    consumindo a api

--- Rocketflix/api.js
+++
@@ -1,6 +0,0 @@
-// Substitua o CHAVE_DA_API com a chave gerada no site.
-
-export const API_KEY = '518da50cadac569271cc9c75401d6702';
-export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-export const language = 'language=pt-BR';
---
+++ Rocketflix/main.js
@@ -0,0 +1,28 @@
+const API_KEY = '52049f468d48721a1489c682170d7877';
+//https://api.themoviedb.org/3/movie/550?api_key=52049f468d48721a1489c682170d7877
+
+/*eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjA0OWY0NjhkNDg3MjFhMTQ4OWM2ODIxNzBkNzg3NyIsInN1YiI6IjYzN2E4YjcxMzM2ZTAxMDBkOTc4YmJjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jlSBzzzNZe6xxa4b_JRLv87rtTY6rijfYNMunoZRX_g*/
+
+
+function getFilme(){
+
+    let id = Math.floor(Math.random() * 1000)
+
+    const URL = `https://api.themoviedb.org/3/movie/${id}?api_key=52049f468d48721a1489c682170d7877`;
+    const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+    const language = 'language=pt-BR';
+
+    axios.get(URL)
+    .then(data => {
+        console.log(data.data)
+        document.getElementById('filme').style = 'display: block'
+        
+    })
+    .catch(error => {
+        console.log(error)
+        window.alert('Tente novamente')
+        if(document.getElementById('filme').style = 'display: block'){
+            document.getElementById('filme').style = 'display: none'
+        }
+    })   
+}
