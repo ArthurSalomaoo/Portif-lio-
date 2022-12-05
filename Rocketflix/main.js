fafacc6de1fda28b8c6fdcce651171bc18804a26
Arthur Salomao <artursalomao004@gmail.com>
2022-12-04 22:56:47 -0300

    .

--- Rocketflix/main.js
+++ Rocketflix/main.js
@@ -1,8 +1,4 @@
 const API_KEY = '52049f468d48721a1489c682170d7877';
-//https://api.themoviedb.org/3/movie/550?api_key=52049f468d48721a1489c682170d7877
-
-/*eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjA0OWY0NjhkNDg3MjFhMTQ4OWM2ODIxNzBkNzg3NyIsInN1YiI6IjYzN2E4YjcxMzM2ZTAxMDBkOTc4YmJjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jlSBzzzNZe6xxa4b_JRLv87rtTY6rijfYNMunoZRX_g*/
-
 
 function getFilme(){
 
