0e8ff7bab331ebf0e048c238c91f9a7dd2fd4828
Arthur Salomao <artursalomao004@gmail.com>
2022-12-10 22:41:59 -0300

    começando o tema escuro

--- Calculator/script.js
+++ Calculator/script.js
@@ -6,8 +6,10 @@ chk.addEventListener('change', () => {
     console.log(chk.checked)
     if(chk.checked) {
         estilo.setAttribute('href', 'estilo/preto.css')
+        document.getElementById('historicoimg').setAttribute('src', 'imagens/historicoBranco.svg')
     } else {
         estilo.setAttribute('href', 'estilo/style.css')
+        document.getElementById('historicoimg').setAttribute('src', 'imagens/historico.svg')
 
     }
 })
